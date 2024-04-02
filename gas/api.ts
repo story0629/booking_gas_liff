d1509d14bd41d38a292b4b6bdb031db21d8b07c3
Tommy.Wu <story0629@gmail.com>
2024-04-03 00:12:26 +0800

    GAS doPost complete

---
+++ gas/api.ts
@@ -0,0 +1,193 @@
+interface typePostContent {
+    // Simplybook
+    // {"booking_id":"8","booking_hash":"f3653d531e887a0be2cd4b3c0ba686b7","company":"akashictommy","notification_type":"create"}
+    booking_id: string;
+    booking_hash: string;
+    company: string;
+    notification_type: 'create' | 'cancel' | 'new_client' | 'change' | 'create_invoice';
+    // LIFF
+    // {"iss": "https://access.line.me", "sub": "U1234567890abcdef1234567890abcdef", "aud": "1234567890", "exp": 1504169092, "iat": 1504263657, "amr": ["pwd"], "name": "Taro Line", "picture": "XXXXXXXXXXXXXXXXXXXXXXXXXX"}
+    iss: string;
+    sub: string;
+    aud: string;
+    exp: number;
+    iat: number;
+    amr: string[];
+    name: string;
+    picture: string;
+    email?: string;
+}
+
+type typePostType = 'Simplybook' | 'LIFF';
+
+interface typeSimplyBookDetail {
+    id: number;
+    client_id: number;
+    code: string;
+    start_datetime: string;
+    end_datetime: string;
+    invoice_datetime: string; // UTC+8
+    payment_received: boolean;
+    service: typeSimplyBookService;
+    client: typeSimplyBookClient;
+    additional_fields: typeSimplyBookAdditionalFields[];
+}
+
+interface typeSimplyBookService {
+    id: number;
+    name: string;
+    price: number;
+}
+
+interface typeSimplyBookClient {
+    id: number;
+    name: string;
+    email: string;
+    phone: string;
+}
+
+interface typeSimplyBookAdditionalFields {
+    id: number;
+    field_name: string;
+    value: string;
+}
+
+const ws = SpreadsheetApp.getActiveSpreadsheet();
+
+
+// Google Apps Script Post Endpoint
+// Step1：get post content
+// Step2：Judeg type by content, if booking_id is exist then type is Simplybook else type is LIFF
+// Step3：Save to content to google sheet (name is webhook_log), content is current_datetime, type, post content, need to insert row in top
+// Step4：Save data to google Sheet
+// Step4：return ok
+const doPost = (e: GoogleAppsScript.Events.DoPost) => {
+    const content: typePostContent = JSON.parse(e.postData.contents);
+
+    const type: typePostType = content.hasOwnProperty('booking_id') ? 'Simplybook' : 'LIFF';
+    const sheet = ws.getSheetByName('webhook_log')!;
+    sheet.insertRowBefore(1);
+    sheet.getRange(1, 1).setValue(new Date());
+    sheet.getRange(1, 2).setValue(type);
+    sheet.getRange(1, 3).setValue(content);
+
+    savePostDataToSheet(type, content);
+    return ContentService.createTextOutput('OK');
+}
+
+// save post data to google sheet
+// Step1：type: Simplybook or LIFF
+// Step2：if type is Simplybook and use booking_id secret_key to send get request to get detail info
+// Step3：if type is Simplybook and notification_type is create then save [index, booking_id, ... , created_at, updated_at] to booking_list
+// Step4: if type is Simplybook and notification_type is not equal create then user booking_id to get row number and update column
+// if tyle is LIFF and sub is not isit in sheet then save [sub, customer_name, aud, exp, email] to cutomer_list
+const savePostDataToSheet = async (type: typePostType, content: typePostContent) => {
+    // Step1
+    if (type === 'Simplybook') {
+        // Step2 先取得 Simplybook 的 詳細資料
+        const { booking_id, notification_type } = content;
+        const booking_detail: typeSimplyBookDetail | null = await getSimplyBookDetail(booking_id);
+
+        if (!booking_detail) {
+            return 'booking_detail is null';
+        }
+
+        const sheet = ws.getSheetByName('booking_list')!
+        if (notification_type === 'create') {
+            // Step3 新增至 Google Sheet
+            simplybookCreate(sheet, booking_detail);
+        } else if (notification_type === 'change') {
+            // Step4 更新 Google Sheet
+            simplybookUpdate(sheet, booking_detail);
+        } else if (notification_type === 'cancel') {
+            // Step4 更新 Google Sheet
+            simplybookCancel(sheet, booking_detail);
+        }
+        // 其他 notification_type 例：new_client / create_invoice 因該不用做任何事，放著
+    } else if (type === 'LIFF') {
+        // Step5 LIFF 的情況
+    }
+}
+
+interface typeRequestOptions {
+    method: 'get' | 'post';
+    headers?: {
+        [key: string]: string;
+    },
+    payload?: {
+        [key: string]: string;
+    }
+}
+
+// Refresh Token 的 Response
+interface typeRefreshResponse {
+    token: string;
+    refresh_token: string
+}
+
+
+// Use rest api to get booking detail
+// Step1：define setting_sheet (sheet_name is setting)
+// Step2：get api_key and secret_key from setting_sheet
+// Step3：use booking_id & key to send get request
+// Step4：return response
+const getSimplyBookDetail = async (booking_id: string) => {
+    const setting_sheet = ws.getSheetByName('setting')!;
+
+    // todo: range 位置需要確認
+    const url: string = setting_sheet.getRange(1, 2).getValue();
+    const api_token: string = setting_sheet.getRange(1, 2).getValue();
+    const company: string = setting_sheet.getRange(1, 2).getValue();
+    // get request, need set key into header
+    const endponint = `${url}/admin/bookings/${booking_id}`;
+    const options: typeRequestOptions = {
+        method: 'get',
+        headers: {
+            "Accept": "*/*",
+            "X-Token": api_token,
+            "X-Company-Login": company
+        }
+    }
+    let result: typeSimplyBookDetail | null = null;
+
+    const response = await sendRequest(endponint, options)
+
+    // if response.code = 401, need to refresh token and get data one more time
+    if (response.getResponseCode() === 200) {
+        result = JSON.parse(response.getContentText());
+    } else if (response.getResponseCode() === 401) {
+        // token 已過期
+        const refresh_endpoint = `${url}/admin/auth/refresh-token`;
+        const refresh_token: string = setting_sheet.getRange(1, 2).getValue();
+        const refresh_options: typeRequestOptions = {
+            method: 'post',
+            payload: {
+                company,
+                refresh_token
+            }
+        }
+        const refresh_response = await sendRequest(refresh_endpoint, refresh_options);
+        if (refresh_response.getResponseCode() === 200) {
+            // update setting_sheet with new token
+            const body: typeRefreshResponse = JSON.parse(refresh_response.getContentText());
+            const { token, refresh_token } = body
+            setting_sheet.getRange(1, 2).setValue(token);
+            setting_sheet.getRange(1, 2).setValue(refresh_token);
+            // get data one more time
+            const response = await sendRequest(endponint, options)
+            response.getResponseCode() === 200 ? result = JSON.parse(response.getContentText()) : result = null;
+        }
+    }
+
+    return result;
+}
+
+const sendRequest = async (endpoint: string, options: typeRequestOptions) => {
+    const response = await UrlFetchApp.fetch(endpoint, options);
+    return response;
+}
+
+const simplybookCreate = (sheet: GoogleAppsScript.Spreadsheet.Sheet, detail: typeSimplyBookDetail) => { }
+const simplybookUpdate = (sheet: GoogleAppsScript.Spreadsheet.Sheet, detail: typeSimplyBookDetail) => { }
+const simplybookCancel = (sheet: GoogleAppsScript.Spreadsheet.Sheet, detail: typeSimplyBookDetail) => { }
+const liffCreate = (content: typePostContent) => { }
