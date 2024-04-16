da52accfa1ff77446f0d61395f451493578a3f6a
Tommy.Wu <story0629@gmail.com>
2024-04-16 23:39:49 +0800

    LIFF app.js 完成

--- scripts/app.js
+++ scripts/app.js
@@ -5,11 +5,11 @@
   // let liffId = "2004166882-6k5LZylq"; // LIFF TEST
 
   await liff.init({ liffId });
-  console.log("Step1: ", "LIFF INIT");
+  // console.log("Step1: ", "LIFF INIT");
 
   // STEP2 check is logined or not
   const isLogined = await liff.isLoggedIn();
-  console.log("Step2: ", isLogined);
+  // console.log("Step2: ", isLogined);
   const goto = getFullGotoParam();
 
   if (!isLogined) {
@@ -23,41 +23,44 @@
   // STEP3 Get user info
   const user_info = await liff.getDecodedIDToken();
 
-  console.log("Step3: ", user_info);
-  console.log("Step3: ", goto);
+  // console.log("Step3: ", user_info);
+  // console.log("Step3: ", goto);
 
   // STEP4 Sent post request
   // payload is user_info & goto concat
   user_info.goto = goto;
-  console.log(user_info);
-  sendPostRequest(user_info);
+  // console.log(user_info);
+  await sendPostRequest(user_info);
   // STEP4 Redirect to goto
   if (typeof goto === "string") location.href = goto;
 })();
 
-const sendPostRequest = async (payload) => {
+const sendPostRequest = (payload) => {
   try {
     // use xhr
-    const xhr = new XMLHttpRequest();
-    xhr.open(
-      "POST",
-      "https://script.google.com/macros/s/AKfycbz0wJxN7J8_5pJU18i4lg8rJl2HcdZVhk4W8QUyv8ICaW7j-7IiBP3TryXaEq3P96xh8A/exec",
-      true
-    );
-    xhr.setRequestHeader("Content-Type", "text/plain");
-    xhr.setRequestHeader("Accept", "*/*");
-    xhr.onreadystatechange = function () {
-      if (xhr.readyState === XMLHttpRequest.DONE && xhr.status === 200) {
-        // 請求成功,可以在這裡處理回應
-        console.log(xhr.responseText);
-      }
-    };
-    xhr.onerror = function () {
-      // 請求發生錯誤
-      console.error(xhr.statusText);
-    };
-    xhr.send(JSON.stringify(payload));
-    // fetch 後不需要得到 response，直接 return
+    return new Promise((resolve, reject) => {
+      const xhr = new XMLHttpRequest();
+      xhr.open(
+        "POST",
+        "https://script.google.com/macros/s/AKfycbz0wJxN7J8_5pJU18i4lg8rJl2HcdZVhk4W8QUyv8ICaW7j-7IiBP3TryXaEq3P96xh8A/exec",
+        true
+      );
+      xhr.setRequestHeader("Content-Type", "text/plain");
+      xhr.setRequestHeader("Accept", "*/*");
+      xhr.onprogress = function () {
+        resolve();
+        // if (xhr.readyState === XMLHttpRequest.DONE && xhr.status === 200) {
+        //   // 請求成功,可以在這裡處理回應
+        //   resolve(xhr.responseText);
+        // }
+      };
+      xhr.onerror = function () {
+        // 請求發生錯誤
+        reject(xhr.statusText);
+      };
+      xhr.send(JSON.stringify(payload));
+      // fetch 後不需要得到 response，直接 return
+    });
   } catch (error) {
     return error;
   }
