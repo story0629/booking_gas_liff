27238fd5cd8078dc370def6ba2c26b840c3aed6f
Tommy.Wu <story0629@gmail.com>
2024-04-24 11:04:50 +0800

    LIFF update

--- scripts/app.js
+++ scripts/app.js
@@ -42,7 +42,7 @@ const sendPostRequest = (payload) => {
       const xhr = new XMLHttpRequest();
       xhr.open(
         "POST",
-        "https://script.google.com/macros/s/AKfycbxpZrMohq4p8WLM207LmBbyVAYsaOCrdqD2knKlGesEbxsAwG7InRWGAFn6-jmaqQYp2Q/exec",
+        "https://script.google.com/macros/s/AKfycbxpZrMohq4p8WLM207LmBbyVAYsaOCrdqD2knKlGesEbxsAwG7InRWGAFn6-jmaqQYp2Q/exechttps://script.google.com/macros/s/AKfycbxyKYUOaxtLAuQRnJMtuNTrCCvAboCnzK1VjGJrfhFUR_SMCkMCkq6_NT24UxbjxdhfBw/exec",
         true
       );
       xhr.setRequestHeader("Content-Type", "text/plain");
